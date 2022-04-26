20fae05ec55bef30a610d4caf5f5afc44ff2aef4
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2022-04-26 08:19:07 +0200

    shit eater

--- dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
+++ dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
@@ -38,8 +38,31 @@ namespace DSI_Mockup
 
         private void Border_Click(object sender, RoutedEventArgs e)
         {
+            if (fireman.BorderThickness != new Thickness(0))
+                fireman.BorderThickness = new Thickness(0);
+            if (Medic.BorderThickness != new Thickness(0))
+                Medic.BorderThickness = new Thickness(0);
+            if (Police.BorderThickness != new Thickness(0))
+                Police.BorderThickness = new Thickness(0);
+            if (Army.BorderThickness != new Thickness(0))
+                Army.BorderThickness = new Thickness(0);
             Button boton = sender as Button;
             boton.BorderBrush = new SolidColorBrush(Colors.LightGreen);
+            boton.BorderThickness = new Thickness(10);
+        }
+
+        private void EneBorder_Click(object sender, RoutedEventArgs e)
+        {
+            if (ene1.BorderThickness != new Thickness(0))
+                ene1.BorderThickness = new Thickness(0);
+            if (ene2.BorderThickness != new Thickness(0))
+                ene2.BorderThickness = new Thickness(0);
+            if (ene3.BorderThickness != new Thickness(0))
+                ene3.BorderThickness = new Thickness(0);
+            if (ene4.BorderThickness != new Thickness(0))
+                ene4.BorderThickness = new Thickness(0);
+            Button boton = sender as Button;
+            boton.BorderBrush = new SolidColorBrush(Colors.LightYellow);
             boton.BorderThickness = new Thickness(10);
         }
     }
