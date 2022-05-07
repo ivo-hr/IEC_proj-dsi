d0ff22c85aee29329bde4d6ea1cd68b112c7b81f
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2022-05-07 20:51:48 +0200

    polla

--- dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
+++ dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.System;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
