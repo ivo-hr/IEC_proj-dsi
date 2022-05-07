a9f73fbbd8806407cd2207763dae8685cea19582
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2022-05-07 19:05:53 +0200

    uke

--- dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
+++ dsi-mockup-pero-en-xaml-xd/Combate.xaml.cs
@@ -49,6 +49,35 @@ namespace DSI_Mockup
             Button boton = sender as Button;
             boton.BorderBrush = new SolidColorBrush(Colors.LightGreen);
             boton.BorderThickness = new Thickness(10);
+
+            switch (boton.Name){
+                case "fireman":
+                    abil1Desc.Text = "Dale un manguerazo y echa a tu enemigo del mapa!";
+                    abil2Desc.Text = "Recobra fuerzas tomándote una barrita energética. Qué rico!";
+                    abil3Desc.Text = "Lanza tu manguera al enemigo, enredándolo y bajando su  defensa!";
+                    specAbilDesc.Text = "Ataca  a tu enemigo con un hacha, haciendo mucho daño!";
+                    break;
+                case "Medic":
+                    abil1Desc.Text = "Dispara a tu enemigo con un extintor para no dejarle ver!";
+                    abil2Desc.Text = "Recupera  un poco de  vida con una tirita. Justo lo que necesitaba!";
+                    abil3Desc.Text = "Dale al enemigo con el extintor, aturdéndole durante un tiempo!";
+                    specAbilDesc.Text = "Lanza tu extintor para que  explote, haciendo daño a todos los enemigos!";
+                    break;
+                case "Police":
+                    abil1Desc.Text = "Ponm la escalera delante tuya para aumentar  tu defensa!";
+                    abil2Desc.Text = "Aumenta tu  velocidad tomando una bebida energética. Fiuum!";
+                    abil3Desc.Text = "Lanza la escalera al pie de tus enemigos, reduciendo la velocidad a  todos!";
+                    specAbilDesc.Text = "Pega un maporrazo con la escalera a 2 de tus enemigos. Qué daño!";
+                    break;
+                case "Army":
+                    abil1Desc.Text = "Da a uno de tus compañeros uno de tus  objetos para que lo use!";
+                    abil2Desc.Text = "Tómate  un cafe para aumentar tu  eficiencia de curación!";
+                    abil3Desc.Text = "Cura un poco a uno de tus aliados!";
+                    specAbilDesc.Text = "Sana por completo a  tus  aliados, además de darles un poco de defensa!";
+                    break;
+
+            }
+            
         }
 
         private void EneBorder_Click(object sender, RoutedEventArgs e)
